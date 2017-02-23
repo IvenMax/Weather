e5bca52996fd6d874fb7eda0d24d55d6823e39f0
IvenMax <yanfengqiu06@163.com>
2017-02-23 17:58:48 +0800

    ...

--- app/src/main/java/com/iven/app/activity/HistoryActivity.java
+++ app/src/main/java/com/iven/app/activity/HistoryActivity.java
@@ -1,20 +1,64 @@
 package com.iven.app.activity;
 
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.iven.app.R;
+import com.iven.app.base.BaseActivity;
 import com.iven.app.bean.HistoryOfTodayBean;
+import com.squareup.picasso.Picasso;
 
-public class HistoryActivity extends AppCompatActivity {
+public class HistoryActivity extends BaseActivity {
     private static final String TAG = "zpy_HistoryActivity";
+    private TextView tv_his_title;
+    private TextView tv_his_time;
+    private TextView tv_his_desc;
+    private ImageView iv_his_pic;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_history);
+    public int setLayout() {
+        return R.layout.activity_history;
+    }
+
+    @Override
+    public void setTitle() {
+        title_title.setText("历史今天");
+        title_left.setCompoundDrawablesWithIntrinsicBounds(getResources().getDrawable(R.mipmap.title_back), null, null, null);
+        title_left.setOnClickListener(this);
+    }
+
+    @Override
+    public void initWidget() {
+        tv_his_title = (TextView) findViewById(R.id.tv_his_title);
+        tv_his_time = (TextView) findViewById(R.id.tv_his_time);
+        tv_his_desc = (TextView) findViewById(R.id.tv_his_desc);
+        iv_his_pic = (ImageView) findViewById(R.id.iv_his_pic);
         HistoryOfTodayBean.ResultBean resultBean = (HistoryOfTodayBean.ResultBean) getIntent().getSerializableExtra("resultBean");
-        Log.e(TAG, "onCreate: 16" + "行 = " + resultBean.toString());
+        setData(resultBean);
+    }
+
+    //数据展示
+    private void setData(HistoryOfTodayBean.ResultBean resultBean) {
+        if (null == resultBean) {
+            return;
+        }
+        tv_his_title.setText(resultBean.getTitle());
+        tv_his_time.setText(String.format("%d年", resultBean.getYear()));
+        if (resultBean.getPic() != null) {
+            Picasso.with(this).load(resultBean.getPic()).into(iv_his_pic);
+        }
+        tv_his_desc.setText(resultBean.getDes());
+    }
+
+    @Override
+    public void widgetClick(View view) {
+        switch (view.getId()) {
+            case R.id.title_left:
+                finish();
+                break;
+            default:
+                break;
+        }
     }
 }
