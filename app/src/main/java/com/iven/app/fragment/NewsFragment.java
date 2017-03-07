4d98b287efe4c1ef608507fe599405d1c0bfe8bd
IvenMax <791689609@qq.com>
2017-03-07 22:06:39 +0800

    ...

--- app/src/main/java/com/iven/app/fragment/NewsFragment.java
+++ app/src/main/java/com/iven/app/fragment/NewsFragment.java
@@ -2,40 +2,27 @@ package com.iven.app.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.iven.app.R;
-import com.iven.app.adapter.TestAdapter;
-
-import java.util.ArrayList;
 
 /**
  * @author Iven
  * @date 2017/2/21 14:43
- * @Description
+ * @Description 新闻页
  */
 
 public class NewsFragment extends Fragment {
-    private RecyclerView mRecyclerView;
-    private ArrayList<String> mStringArrayList=new ArrayList<>();
+    private TabLayout mTabs;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.layout_fragment_news, container, false);
-        mRecyclerView = ((RecyclerView) view.findViewById(R.id.test_rrrrr));
-        for (int i = 0; i < 30; i++) {
-            mStringArrayList.add("sssss"+i);
-        }
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
-        linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
-        mRecyclerView.setLayoutManager(linearLayoutManager);
-        mRecyclerView.setAdapter(new TestAdapter(mStringArrayList,getActivity()));
         return view;
     }
 
