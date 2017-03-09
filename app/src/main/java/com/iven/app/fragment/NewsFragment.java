47fe543f50c393a03e98ede274f540248b01d8f6
IvenMax <791689609@qq.com>
2017-03-09 21:25:51 +0800

    swiperefreshlayout下拉刷新

--- app/src/main/java/com/iven/app/fragment/NewsFragment.java
+++ app/src/main/java/com/iven/app/fragment/NewsFragment.java
@@ -1,6 +1,8 @@
 package com.iven.app.fragment;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -45,6 +47,7 @@ public class NewsFragment extends BaseFragment {
     private NewLoadingUtil mNewLoadingUtil;
     private int startPage = 0;//起始加载索引
     private String id;
+    private SwipeRefreshLayout swiperefreshlayout;
 
 
     @Override
@@ -54,8 +57,11 @@ public class NewsFragment extends BaseFragment {
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
         linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         rv_new_list.setLayoutManager(linearLayoutManager);
+        swiperefreshlayout = (SwipeRefreshLayout) view.findViewById(R.id.swiperefreshlayout);
         datas = new ArrayList<>();
         rv_new_list.setItemAnimator(new DefaultItemAnimator());
+        swiperefreshlayout.setColorSchemeColors(Color.RED, Color.BLUE);
+
         mNewsListAdapter = new NewsListAdapter(getActivity(), datas);
         rv_new_list.setAdapter(mNewsListAdapter);
         setListener();
@@ -63,6 +69,15 @@ public class NewsFragment extends BaseFragment {
     }
 
     private void setListener() {
+        //下拉刷新
+        swiperefreshlayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                Log.e(TAG, "onRefresh: 76" + "行 = " +"下拉刷新中.......");
+                startPage=0;
+                http_request("list",id,startPage);
+            }
+        });
         mNewsListAdapter.setOnItemClickLitener(new NewsListAdapter.OnItemClickLitener() {
             @Override
             public void onItemClick(View view, int position) {
@@ -79,33 +94,33 @@ public class NewsFragment extends BaseFragment {
             }
         });
         //滚动监听,实现下拉刷新效果
-//        rv_new_list.addOnScrollListener(new RecyclerView.OnScrollListener() {
-//            @Override
-//            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
-//                super.onScrollStateChanged(recyclerView, newState);
-//                RecyclerView.LayoutManager layoutManager = recyclerView.getLayoutManager();
-//                int lastVisibleItemPosition = ((LinearLayoutManager) layoutManager).findLastVisibleItemPosition();
-//                int visibleItemCount = layoutManager.getChildCount();
-//                int totalItemCount = layoutManager.getItemCount();
-//
-//                if (visibleItemCount > 0 && newState == RecyclerView.SCROLL_STATE_IDLE
-//                        && lastVisibleItemPosition >= totalItemCount - 1) {
-//                    startPage+=20;
-//                    Log.e(TAG, "onScrollStateChanged: 96" + "行 = " +startPage);
-//                    http_request("list",id,startPage);
-//                    mNewsListAdapter.showFooter();
-//                    rv_new_list.scrollToPosition(mNewsListAdapter.getItemCount() - 1);
-//                }
-//            }
-//
-//        });
+        //        rv_new_list.addOnScrollListener(new RecyclerView.OnScrollListener() {
+        //            @Override
+        //            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+        //                super.onScrollStateChanged(recyclerView, newState);
+        //                RecyclerView.LayoutManager layoutManager = recyclerView.getLayoutManager();
+        //                int lastVisibleItemPosition = ((LinearLayoutManager) layoutManager).findLastVisibleItemPosition();
+        //                int visibleItemCount = layoutManager.getChildCount();
+        //                int totalItemCount = layoutManager.getItemCount();
+        //
+        //                if (visibleItemCount > 0 && newState == RecyclerView.SCROLL_STATE_IDLE
+        //                        && lastVisibleItemPosition >= totalItemCount - 1) {
+        //                    startPage+=20;
+        //                    Log.e(TAG, "onScrollStateChanged: 96" + "行 = " +startPage);
+        //                    http_request("list",id,startPage);
+        //                    mNewsListAdapter.showFooter();
+        //                    rv_new_list.scrollToPosition(mNewsListAdapter.getItemCount() - 1);
+        //                }
+        //            }
+        //
+        //        });
     }
 
     @Override
     public void initData() {
         title = getArguments().getString("title", "0");
         if (!TextUtils.isEmpty(title)) {
-             id = getNewsId(title);
+            id = getNewsId(title);
             http_request("list", id, startPage);
         } else {
             Log.e(TAG, "initData: 32" + "行 = index = " + title);
@@ -127,6 +142,7 @@ public class NewsFragment extends BaseFragment {
             public void onCompleted() {
                 Log.e(TAG, "onCompleted: 103" + "行 = ");
                 mNewLoadingUtil.stopShowLoading();
+                swiperefreshlayout.setRefreshing(false);
             }
 
             @Override
@@ -134,6 +150,7 @@ public class NewsFragment extends BaseFragment {
                 Log.e(TAG, "onError: 108" + "行 = ");
                 T.showShort(getActivity(), e.getMessage());
                 mNewLoadingUtil.stopShowLoading();
+                swiperefreshlayout.setRefreshing(false);
             }
 
 
